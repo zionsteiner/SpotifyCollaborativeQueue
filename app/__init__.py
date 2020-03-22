973c4d9bc9cbd389e6681e653bab6a4740072622
Zion <zion.steiner@aggiemail.usu.edu>
2020-03-21 21:52:05 -0400

    Added heroku config

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,10 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from spotipy import oauth2
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'pxR5xFaq33ytBAg'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.abspath(os.path.dirname(__file__)) + 'app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SPOTIFY_CLIENT_ID'] = '53047d625f1848f79d80c0092ee02489'
-app.config['SPOTIFY_CLIENT_SECRET'] = 'e7c233c83cc74124bd861cfde77f0d86'
-app.config['REDIRECT_URI'] = 'http://127.0.0.1:5000/login'
-app.config['SCOPE'] = 'user-modify-playback-state'
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirnmae(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    SECRET_KEY = os.environ['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
+    SPOTIFY_CLIENT_ID = os.environ['SPOTIFY_CLIENT_ID']
+    SPOTIFY_CLIENT_SECRET = os.environ['SPOTIFY_CLIENT_SECRET']
+    REDIRECT_URI = os.environ['REDIRECT_URI']
+    SCOPE = os.environ['SCOPE']
+    LOG_TO_STDOUT = os.environ['LOG_TO_STDOUT']
